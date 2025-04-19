ce26ea78ff3781051255e8a61d4b2801e47eedf1
KulpithaC <KulpithaC@gmail.com>
2025-04-19 16:45:37 +0700

    Initialize backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+// const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
+
+// // Connect to MongoDB
+// mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.error('MongoDB connection error:', err));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
